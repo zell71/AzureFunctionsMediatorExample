9185afd28e67132a331bbfda4a6c6dd8a43034e7
djones <david.jones@bupa.com.au>
2020-08-26 10:08:11 +1000

    missing files

---
+++ src/FunctionsAppSample.Function/Features/Packages/Create.cs
@@ -0,0 +1,69 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FluentValidation;
+using FunctionsAppSample.Function.Domain;
+using MediatR;
+
+namespace FunctionsAppSample.Function.Features.Packages
+{
+    public class Create
+    {
+        public class PackageData
+        {
+            public string ExId { get; set; }
+
+            public string Title { get; set; }
+
+            public string Description { get; set; }
+        }
+
+        public class PackageDataValidator : AbstractValidator<PackageData>
+        {
+            public PackageDataValidator()
+            {
+                RuleFor(x => x.ExId).NotNull().NotEmpty();
+                RuleFor(x => x.Title).NotNull().NotEmpty();
+                RuleFor(x => x.Description).NotNull().NotEmpty();
+            }
+        }
+
+        public class Command : IRequest<PackageEnvelope>
+        {
+            public Command(string title, string description)
+            {
+                Package = new PackageData
+                {
+                    Title = title,
+                    Description = description
+                };
+            }
+            public PackageData Package { get; set; }
+        }
+
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(x => x.Package).NotNull().SetValidator(new PackageDataValidator());
+            }
+        }
+
+        public class Handler : IRequestHandler<Command, PackageEnvelope>
+        {
+            public Handler()
+            {
+            }
+
+            public Task<PackageEnvelope> Handle(Command message, CancellationToken cancellationToken)
+            {
+                var package = new Package
+                {
+                    Title = message.Package.Title,
+                    Description = message.Package.Description
+                };
+
+                return Task.FromResult(new PackageEnvelope(package));
+            }
+        }
+    }
+}
---
+++ src/FunctionsAppSample.Function/Features/Packages/PackageEnvelope.cs
@@ -0,0 +1,14 @@
+﻿using FunctionsAppSample.Function.Domain;
+
+namespace FunctionsAppSample.Function.Features.Packages
+{
+    public class PackageEnvelope
+    {
+        public PackageEnvelope(Package package)
+        {
+            Package = package;
+        }
+
+        public Package Package { get; }
+    }
+}
